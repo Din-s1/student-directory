287b9dbd3fe11df308d1294d479be78d7d64ae8c
Din <din_stolak@hotmail.co.uk>
2018-08-28 10:57:06 +0100

    We have now written methods that can be used to call our variables, this helps as it leaves our code in a more readable and self-explanatory format and makes it more re-usable.

--- directory.rb
+++ directory.rb
@@ -1,23 +1,34 @@
 # List of students in the academy
 # Students are now placed in a list (Array)
 students = [
-   "Dr. Hannibal Lecter"
-   "Darth Vader"
-   "Nurse Ratched"
-   "Michael Corleone"
-   "Alex DeLarge"
-   "The Wicked Witch of the West"
-   "Terminator"
-   "Freddy Krueger"
-   "The Joker"
-   "Joffrey Baratheon"
-   "Norman Bates"
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
 ]
-# Now we print the list
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(names)
+  names.each do |name|
+  puts name
+  end
 end
 # finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students."
+end
+#These new methods can now be called so they are in action
+print_header
+print(students)
+print_footer(students)
