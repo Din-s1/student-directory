5d07990fdecf4b0494cea629d9e61336e3254717
Din <din_stolak@hotmail.co.uk>
2018-08-29 11:08:28 +0100

    The list now takes names and hobbies, it also uses a while loop instead of the each() method and selects only names begining with a capital s and less than 12 characters long.

--- directory.rb
+++ directory.rb
@@ -1,19 +1,23 @@
 # List of students in the academy
 # Students are now placed in a list (Array)
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the name of the student followed then hit enter and enter their hobby"
+  puts "To finish, just hit return thrice"
   #create and empty array
   students = []
   #get the first name
   name = gets.chomp
+  hobby = gets.chomp
+
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobby: hobby}
     puts "Now we have #{students.count} students"
+    
     #get another name from the user
     name = gets.chomp
+    hobby = gets.chomp
   end
   #return the array of students
   students
@@ -25,8 +29,10 @@ def print_header
 end
 
 def print(students)
-    students.each do |student|
-      puts "#{students.index(student) + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  iterator = 0
+  while students.select! { |val| val[:name].start_with?("S") && (val[:name].length < 12)}
+    puts "#{iterator + 1}. #{students[iterator][:name]} #{students[iterator][:hobby]} (#{students[iterator][:cohort]} cohort)"
+    iterator += 1
   end
 end
 # finally, we print the total number of students
