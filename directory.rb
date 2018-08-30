910bc2210ca770f201760a121da7bd9044a577d6
Din <din_stolak@hotmail.co.uk>
2018-08-30 11:26:43 +0100

    The student directory now takes other info about the students. An attempt has been started where it will take user-input to correct a typo.

--- directory.rb
+++ directory.rb
@@ -1,25 +1,15 @@
-# List of students in the academy
-# Students are now placed in a list (Array)
 def input_students
   puts "Please enter the name of the student followed then hit enter and enter their hobby"
   puts "To finish, just hit return thrice"
-  #create and empty array
   students = []
-  #get the first name
-  name = gets.chomp
-  hobby = gets.chomp
-
-  #while the name is not empty, repeat this code
-  while !name.empty? do
-    #add the student hash to the array
-    students << {name: name, cohort: :november, hobby: hobby}
+  name, cohort, hobby = gets.chomp, gets.chomp, gets.chomp
+  while !name.empty?
+    cohort = "Novembuary" if cohort == ""
+    hobby = "Fun" if hobby == ""
+    students << {name: name.capitalize, cohort: cohort, hobby: hobby}
     puts "Now we have #{students.count} students"
-    
-    #get another name from the user
-    name = gets.chomp
-    hobby = gets.chomp
+    name, cohort, hobby = gets.chomp, gets.chomp, gets.chomp
   end
-  #return the array of students
   students
 end
 
@@ -32,16 +22,29 @@ def print(students)
   iterator = 0
   while students.select! { |val| val[:name].start_with?("S") && (val[:name].length < 12)}
     until iterator == students.length
-      puts "#{iterator + 1}. #{students[iterator][:name]} #{students[iterator][:hobby]} (#{students[iterator][:cohort]} cohort)"
+      puts "#{iterator + 1}. #{students[iterator][:name]}, #{students[iterator][:hobby]}, (#{students[iterator][:cohort]} cohort)"
       iterator += 1
     end
   end
 end
-# finally, we print the total number of students
+
+=begin
+an attempt to create a method that gets user input and
+ selects the hash value that is a typo and needs to be replaced
+ *approach: will get user input to iterate through and match value then using select
+ will replace the original value. CI thought... what if we have the same name twice
+ John Smith?
+def typo
+  puts "Please enter the typo as shown in the list"
+  mistake = gets.chomp
+  students.replace {|typo| typo[mistake] }
+end
+=end
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students."
 end
-#These new methods can now be called so they are in action
+
 students = input_students
 print_header
 print(students)
